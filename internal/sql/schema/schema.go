6a031289074937b14947bf166d136007ee359a0b
Matt Bonnell <matt.bonnell@icloud.com>
2021-02-16 11:25:19 -0500

    remove panics

--- internal/sql/schema/schema.go
+++ internal/sql/schema/schema.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/mattbonnell/gq/internal/sql/schema/mysql"
@@ -20,10 +21,22 @@ func getSchema(db *sqlx.DB) []string {
 	}
 }
 
-func CreateSchema(db *sqlx.DB) {
+func CreateSchema(db *sqlx.DB) error {
 	log.Debug().Msg("creating schema")
+	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to begin tx: %s", err)
+	}
+	defer tx.Rollback()
 	for _, stmt := range getSchema(db) {
-		db.MustExec(stmt)
+		_, err := tx.Exec(stmt)
+		if err != nil {
+			return fmt.Errorf("failed to exec stmt %s: %s", strings.Split(stmt, "(")[0], err)
+		}
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit tx: %s", err)
 	}
 	log.Debug().Msg("schema created")
+	return nil
 }
--- internal/sql/schema/schema_test.go
+++ internal/sql/schema/schema_test.go
@@ -9,13 +9,9 @@ import (
 )
 
 func TestCreateSchema_sqlite3(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Fatalf("failed to create schema: %s\n", r)
-		}
-	}()
 	db, err := sqlx.Connect("sqlite3", ":memory:")
 	require.NoError(t, err, "failed to connect to db")
 
-	CreateSchema(db)
+	err = CreateSchema(db)
+	require.NoError(t, err)
 }
