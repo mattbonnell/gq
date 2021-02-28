47070d240329b36b00d341b9385a2a194c3f530d
Matt Bonnell <matt.bonnell@icloud.com>
2021-02-27 21:55:55 -0500

    fix pg schema

--- internal/databases/postgres/schema.go
+++ internal/databases/postgres/schema.go
@@ -2,13 +2,13 @@ package postgres
 
 const (
 	messageTable = `CREATE TABLE IF NOT EXISTS message (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	payload BLOB NOT NULL,
+	id SERIAL PRIMARY KEY,
+	payload BYTEA NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	ready_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	retries INT DEFAULT 0
 );`
-	messageReadyAtIndex = `CREATE INDEX ready_at ON message (ready_at ASC);`
+	messageReadyAtIndex = `CREATE INDEX IF NOT EXISTS ready_at ON message (ready_at ASC);`
 )
 
 var Schema = []string{messageTable, messageReadyAtIndex}
